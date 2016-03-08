e9264c1ac63826b24991a16cd76a4aeac15a3ae4
nal luksic <nl0243@student.uni-lj.si>
2016-03-08 11:21:03 +0000

    Dodajanje opomnikov

--- opomniki/skripta.js
+++ opomniki/skripta.js
@@ -6,8 +6,26 @@ window.addEventListener('load', function() {
 			document.querySelector("#uporabnik").innerHTML = uporabnik;
 			document.querySelector(".pokrivalo").style.visibility = "hidden";
 		}
-	document.querySelector("#prijavniGumb")
-			.addEventListener('click', izvediPrijavo);
+		document.querySelector("#prijavniGumb")
+				.addEventListener('click', izvediPrijavo);
+			
+			var dodajOpomnik = function(event){
+				var naziv_opomnika = document.querySelector("#naziv_opomnika").value;
+				var cas_opomnika = document.querySelector("#cas_opomnika").value;
+				document.querySelector("#naziv_opomnika").value = "";
+				document.querySelector("#cas_opomnika").value = "";
+				
+				document.querySelector("#opomniki").innerHTML += " \
+				<div class='opomnik'> \
+					<div class='naziv_opomnika'>" + naziv_opomnika + "</div> \
+					<div class='cas_opomnika'>Opomnik čez <span>" + cas_opomnika + 
+						"</span> sekund.</div> \
+						</div>"
+			}
+			
+		document.querySelector("#dodajGumb")
+				.addEventListener('click', dodajOpomnik);
+				
 	//Posodobi opomnike
 	var posodobiOpomnike = function() {
 		var opomniki = document.querySelectorAll(".opomnik");
